f009182938a31ebe4caba8afd50451978640ef85
Maggie <maggie@MacBook-Pro-de-orcas.local>
2023-06-16 14:00:56 +0100

    16Jun2023_14h00

--- rotate.c
+++ rotate.c
@@ -6,9 +6,17 @@
 /*   By: maggie <maggie@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/16 11:33:29 by maggie            #+#    #+#             */
-/*   Updated: 2023/06/16 11:33:35 by maggie           ###   ########.fr       */
+/*   Updated: 2023/06/16 11:46:09 by maggie           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+void	ft_ra(t_list **head_a)
+{
+	t_list	**last;
+	*last = ft_lstlast(*head_a);
+	(*last)->next = head_a;
+	(*head_a)->next = NULL;
+	ft_printf("ra\n");
+}
